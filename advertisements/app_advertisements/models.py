688cc4e5b172d8676f181537a7f93f4e8ed6c60d
Дмитрий <dimongamedeveloper@yandex.ru>
2023-07-26 21:22:54 +0300

    commit

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisements(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Отметьте, если будет уместен торг")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{[self.title, self.description, self.price, self.auction, self.created_at, self.updated_at]!r}'
+    
+    class Meta:
+        db_table = '"advertisements"'
