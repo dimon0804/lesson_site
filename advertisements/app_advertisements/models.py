07806d4e522a94395d67e0ecb6732e6b598af76c
Дмитрий <dimongamedeveloper@yandex.ru>
2023-08-03 16:34:25 +0300

    commit

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 class Advertisements(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -7,6 +8,28 @@ class Advertisements(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если будет уместен торг")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; forn-weight: bold">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    @admin.display(description="Дата редактирования")
+    def up_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: #008080; forn-weight: bold">Сегодня в {}</span>', created_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f'{[self.title, self.description, self.price, self.auction, self.created_at, self.updated_at]!r}'
